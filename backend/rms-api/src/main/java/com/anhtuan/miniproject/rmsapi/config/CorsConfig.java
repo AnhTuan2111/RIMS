7165c5fec93e13a2a5b682ec30618be709bccfda
AnhTuan2111 <anhtuan2006.net@gmail.com>
2026-06-02 14:47:14 +0700

    Add more code base to develop branch
    Create all empty entity in entity package + add mapstruct dependency in pom.xml. we will use lombok + mapstruct to reduce coding times and boiler-plate code lines

--- backend/rms-api/src/main/java/com/anhtuan/miniproject/rmsapi/config/CorsConfig.java
+++ backend/rms-api/src/main/java/com/anhtuan/miniproject/rmsapi/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.anhtuan.miniproject.rmsapi;
+package com.anhtuan.miniproject.rmsapi.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
