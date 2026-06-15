d021677bdcfa877e4e1e1491dbb5e8e723ae8aa2
Tuấn Anh Phạm <phamtuananh937053@gmail.com>
2026-06-15 23:59:50 +0700

    cash v2
    sửa cái checkout bằng cash phải có nhập tiền với tiền trả lại

--- backend/rims-api/src/main/java/vn/edu/fpt/swp391/g6/rimsapi/dto/response/PaymentResponse.java
+++ backend/rims-api/src/main/java/vn/edu/fpt/swp391/g6/rimsapi/dto/response/PaymentResponse.java
@@ -2,6 +2,8 @@ package vn.edu.fpt.swp391.g6.rimsapi.dto.response;
 
 import lombok.*;
 
+import java.math.BigDecimal;
+
 
 @Getter
 @Data
@@ -10,4 +12,7 @@ public class PaymentResponse
     private String message;
     private boolean success;
     private Long invoiceId;
+
+    private BigDecimal amountPaid;
+    private BigDecimal excessAmount;
 }
