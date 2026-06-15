d3d1144b6c0a51c583ca3596ab94ad74bcbc3448
AnhTuan2111 <anhtuan2006.net@gmail.com>
2026-06-15 17:59:04 +0700

    iter 2.1
    view all tables

--- backend/rims-api/src/main/java/vn/edu/fpt/swp391/g6/rimsapi/controller/WaiterController.java
+++ backend/rims-api/src/main/java/vn/edu/fpt/swp391/g6/rimsapi/controller/WaiterController.java
@@ -1,8 +1,14 @@
 package vn.edu.fpt.swp391.g6.rimsapi.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import vn.edu.fpt.swp391.g6.rimsapi.dto.response.TableDetailResponse;
+import vn.edu.fpt.swp391.g6.rimsapi.service.WaiterService;
+
+import java.util.List;
 
 
 @RestController
@@ -10,5 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class WaiterController
 {
+    private final WaiterService waiterService;
 
+    @GetMapping("tables")
+    public ResponseEntity<List<TableDetailResponse>> getWaiterTables()
+    {
+        return ResponseEntity.ok(waiterService.getAllTables());
+    }
 }
--- backend/rims-api/src/main/java/vn/edu/fpt/swp391/g6/rimsapi/service/impl/WaiterServiceImpl.java
+++ backend/rims-api/src/main/java/vn/edu/fpt/swp391/g6/rimsapi/service/impl/WaiterServiceImpl.java
@@ -1,8 +1,37 @@
 package vn.edu.fpt.swp391.g6.rimsapi.service.impl;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import vn.edu.fpt.swp391.g6.rimsapi.dto.response.TableDetailResponse;
+import vn.edu.fpt.swp391.g6.rimsapi.entity.RestaurantTable;
+import vn.edu.fpt.swp391.g6.rimsapi.repository.RestaurantTableRepository;
 import vn.edu.fpt.swp391.g6.rimsapi.service.WaiterService;
 
+import java.util.ArrayList;
+import java.util.List;
 
+
+@Service
+@RequiredArgsConstructor
 public class WaiterServiceImpl implements WaiterService
 {
+    private final RestaurantTableRepository restaurantTableRepository;
+
+    @Override
+    public List<TableDetailResponse> getAllTables()
+    {
+        List<RestaurantTable> tables = restaurantTableRepository.findAll();
+        List<TableDetailResponse> tableDetailResponses = new ArrayList<>();
+        for (RestaurantTable table : tables) {
+            tableDetailResponses.add(
+                    TableDetailResponse.builder()
+                            .tableId(table.getId())
+                            .tableNumber(table.getTableNumber())
+                            .capacity(table.getCapacity())
+                            .status(table.getStatus())
+                            .build());
+        }
+        return tableDetailResponses;
+    }
+
 }
