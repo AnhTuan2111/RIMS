d39847c495d904a655b8a376b99380a537f0d168
AnhTuan2111 <anhtuan2006.net@gmail.com>
2026-06-21 03:11:35 +0700

    Revert "Merge branch 'feature/admin' into develop"
    This reverts commit e5d132ca9c5beafc34b61434a7fe9a3e7f827fdd, reversing
    changes made to 4748e4ab685cb9c6bbde8ab90da43b4b56e48d66.

--- backend/rims-api/src/main/java/vn/edu/fpt/swp391/g6/rimsapi/repository/DishRepository.java
+++ backend/rims-api/src/main/java/vn/edu/fpt/swp391/g6/rimsapi/repository/DishRepository.java
@@ -4,27 +4,12 @@ import vn.edu.fpt.swp391.g6.rimsapi.entity.Dish;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
-
 import java.util.List;
 
 
 @Repository
 public interface DishRepository extends JpaRepository<Dish, Integer>
 {
-    List<Dish> findByCategoryId(Integer categoryId);
-
     List<Dish> findByIsAvailableTrue();
-
     long countByIsAvailableFalse();
-
-    @Query("SELECT d FROM Dish d WHERE LOWER(d.name) LIKE LOWER(CONCAT('%', :keyword, '%'))")
-    List<Dish> searchByName(@Param("keyword") String keyword);
-
-    boolean existsByName(String name);
-
-    boolean existsByNameAndIdNot(String name, Integer id);
-
-    List<Dish> findByCategoryIdAndIsAvailableTrue(Integer categoryId);
 }
