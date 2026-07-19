d50e793c43ecc424a550bb6499981a1f0caadb1b
AnhTuan2111 <anhtuan2006.net@gmail.com>
2026-07-19 13:25:17 +0700

    delete frontend to prepare merge

--- frontend/src/components/waiter/utils.ts
+++
@@ -1,7 +0,0 @@
-export function fmtPrice(p: number | null | undefined) {
-    return (p ?? 0).toLocaleString("vi-VN") + " ₫";
-}
-
-export function toMs(date: string, time: string) {
-    return new Date(`${date}T${time}:00`).getTime();
-}
